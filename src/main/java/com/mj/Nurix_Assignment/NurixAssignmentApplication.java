47413e579316aaf76584bc031e283ca7ab41861e
Mihir <mjmihir9617@gmail.com>
2025-12-06 10:42:34 +0530

    init

---
+++ src/main/java/com/mj/Nurix_Assignment/NurixAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.mj.Nurix_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurixAssignmentApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(NurixAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mj/Nurix_Assignment/NurixAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.Nurix_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurixAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
